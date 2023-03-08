bb7f431c51ec56cc9fc3bd8c2d7b2bc245edf3c3
DreamDev0122 <81429165+DreamDev0122@users.noreply.github.com>
2023-03-08 08:46:30 -0500

    init

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17", 
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      },
+    ],
+  },
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_URL,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
