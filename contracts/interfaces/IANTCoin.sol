9e279065006b2844ed6ddf1501a1879c599a94f0
happybounty <oliveodobounty@gmail.com>
2023-03-09 06:56:07 -0500

    Add-Interface-for-AntCoin

---
+++ contracts/interfaces/IANTCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.13;
+
+interface IANTCoin {
+
+  function mint(
+    address receipt,
+    uint256 _amount
+  ) external;
+
+  function burn(
+    address receipt,
+    uint256 _amount
+  ) external;
+
+  function balanceOf(address account) external returns (uint256);
+}
