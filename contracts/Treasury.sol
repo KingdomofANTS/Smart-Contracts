daf4449de3ee835d4bd147b9cb8d4252052b7c8a
happybounty <oliveodobounty@gmail.com>
2023-03-31 14:36:41 -0400

    treasury smart  contract v2

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -46,13 +46,16 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/Pausable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import './interfaces/IUniswapV2Router02.sol';
 import './interfaces/IUniswapV2Pair.sol';
 import './interfaces/IWETH.sol';
 
 contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
+
+    using SafeMath for uint256;
 
     struct Asset {
         address asset;
@@ -173,21 +176,29 @@ contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
 
     function _calcTotalValueOfTreasury() internal view returns(uint256 _usdAmount) {
         // total ant coin value
-        address[] memory _antCMaticPair;
-        _antCMaticPair[0] = uniswapV2Router.WETH();
-        _antCMaticPair[1] = antCToken;
-        uint256[] memory _antCAmountsIn = uniswapV2Router.getAmountsIn(IERC20(antCToken).balanceOf(address(this)), _antCMaticPair);
-        (, int256 _maticUSDPrice, , , ) = AggregatorV3Interface(maticUSDAggregater).latestRoundData();
-        _usdAmount += _antCAmountsIn[0] * uint256(_maticUSDPrice) * 1000 / (PRECISION * PRECISION_E8);
+        _usdAmount += _calcANTCUSDValue(IERC20(antCToken).balanceOf(address(this)));
         // total LP value
         uint256 totalLPTokenAmount = IERC20(antCLPToken).balanceOf(address(this));
         _usdAmount += totalLPTokenAmount * _calcLPTokenUSDPrice() * 1000 / IERC20Metadata(antCLPToken).decimals();
         // total treasury assets value
         for(uint256 i; i < _activeAssets.length; i++) {
             uint256 _assetBalance = IERC20(_activeAssets[i].asset).balanceOf(address(this));
-            (, int _asssetUSDPrice, , , ) = AggregatorV3Interface(_activeAssets[i].aggregator).latestRoundData();
-            _usdAmount += _assetBalance * uint256(_asssetUSDPrice) * 1000 / (PRECISION * 10**IERC20Metadata(_activeAssets[i].asset).decimals());
+            (, int _assetUSDPrice, , , ) = AggregatorV3Interface(_activeAssets[i].aggregator).latestRoundData();
+            _usdAmount += _assetBalance * uint256(_assetUSDPrice) * 1000 / (PRECISION * 10**IERC20Metadata(_activeAssets[i].asset).decimals());
         }
+    }
+
+    /**
+    * @notice Return a usd price of the ant coin amount
+    */
+
+    function _calcANTCUSDValue(uint256 _antCAmount) internal view returns(uint256) {
+        address[] memory _antCMaticPair;
+        _antCMaticPair[0] = uniswapV2Router.WETH();
+        _antCMaticPair[1] = antCToken;
+        uint256[] memory _antCAmountsIn = uniswapV2Router.getAmountsIn(_antCAmount, _antCMaticPair);
+        (, int256 _maticUSDPrice, , , ) = AggregatorV3Interface(maticUSDAggregater).latestRoundData();
+        return _antCAmountsIn[0] * uint256(_maticUSDPrice) * 1000 / (PRECISION * PRECISION_E8);
     }
 
     /**
@@ -207,11 +218,17 @@ contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
         uint256 tokenAPrice = getTokenUSDPriceForLP(tokenA, reservesA);
         // Get the USD value of TokenB
         uint256 tokenBPrice = getTokenUSDPriceForLP(tokenB, reservesB);
-        lpTokenPrice = (tokenAPrice * reservesA * 2) / lpTotalSupply;
-        lpTokenPrice += (tokenBPrice * reservesB * 2) / lpTotalSupply;
+        lpTokenPrice = (tokenAPrice * reservesA * 2) * 1000 / lpTotalSupply;
+        lpTokenPrice += (tokenBPrice * reservesB * 2) * 1000 / lpTotalSupply;
         
         return lpTokenPrice;
     }
+
+    /**
+    * @notice Return a usd price for one LP token
+    * @param token the token address of LP pair
+    * @param reserve the reserved token amount
+    */
 
     function getTokenUSDPriceForLP(address token, uint256 reserve) internal view returns (uint256) {
         if (token == uniswapV2Router.WETH()) {
@@ -228,9 +245,87 @@ contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
         }
     }
 
+    /**
+    * @notice Return usd price of MATIC
+    * @param _amount matic amount to get the usd price
+    */
+
     function _getMaticUSDPrice(uint256 _amount) internal view returns(uint256) {
         (, int256 _maticUSDPrice, , , ) = AggregatorV3Interface(maticUSDAggregater).latestRoundData();
         return uint256(_maticUSDPrice) * _amount;
+    }
+
+    /**
+    * @notice Return asset usd price regarding the asset info
+    * @param _assetId asset id of treasury active assets
+    */
+
+    function _getAssetUSDPrice(uint256 _assetId) internal view returns(uint256) {
+        Asset memory _asset = _activeAssets[_assetId];
+        (, int256 _usdPrice, , , ) = AggregatorV3Interface(_asset.aggregator).latestRoundData();
+        return uint256(_usdPrice);
+    }
+
+    /**
+    * @notice Return the current treasury pools(antc, antlp, assets) value percentage. e.g. 450 = 4.5%, 200 = 2%
+    */
+
+    function _getCurrentTreasuryHelds() public view returns(uint256, uint256, uint256) {
+        // total ant coin value
+        uint256 antCValue = _calcANTCUSDValue(IERC20(antCToken).balanceOf(address(this)));
+        // total LP value
+        uint256 totalLPTokenAmount = IERC20(antCLPToken).balanceOf(address(this));
+        uint256 antLPValue = totalLPTokenAmount * _calcLPTokenUSDPrice() * 1000 / IERC20Metadata(antCLPToken).decimals();
+        // total treasury assets value
+        uint256 assetsValue;
+        for(uint256 i; i < _activeAssets.length; i++) {
+            uint256 _assetBalance = IERC20(_activeAssets[i].asset).balanceOf(address(this));
+            (, int _assetUSDPrice, , , ) = AggregatorV3Interface(_activeAssets[i].aggregator).latestRoundData();
+            assetsValue += _assetBalance * uint256(_assetUSDPrice) * 1000 / (PRECISION * 10**IERC20Metadata(_activeAssets[i].asset).decimals());
+        }
+        uint256 totalValue = antCValue.add(antLPValue).add(assetsValue);
+        return (antCValue.mul(100).div(totalValue), antLPValue.mul(100).div(totalValue), assetsValue.mul(100).div(totalValue));
+    }
+
+    function _antCTransfer(uint256 _antCUSDForPay, address _sender) internal {
+        // ant coin transfer
+        uint256 _maticUSDPrice = _getMaticUSDPrice(1);
+        uint256 _maticAmountForANTC = _antCUSDForPay.mul(PRECISION_E8).div(_maticUSDPrice);
+        
+        address[] memory _antCMaticPair = new address[](2);
+        
+        _antCMaticPair[0] = uniswapV2Router.WETH();
+        _antCMaticPair[1] = antCToken;
+        
+        uint256[] memory _antCAmountsOut = uniswapV2Router.getAmountsOut(_maticAmountForANTC.mul(PRECISION).div(100), _antCMaticPair);
+        IERC20Metadata(antCToken).transfer(_sender, _antCAmountsOut[1]);
+    }
+
+    function _antLPTransfer(uint256 antCLPUSDForPay, address sender) internal {
+        uint256 lptokenUSDPrice = _calcLPTokenUSDPrice();
+        uint256 lpTokenAmountForPay = antCLPUSDForPay.div(lptokenUSDPrice);
+        IERC20(antCLPToken).transfer(sender, lpTokenAmountForPay);
+    }
+
+    function _assetsTransfer(uint256 assetsUSDForPay, address sender) internal {
+        uint256 assetsTotalValue = 0;
+        uint256[] memory assetHeldRate = new uint256[](_activeAssets.length);
+        for (uint256 i = 0; i < _activeAssets.length; i++) {
+            address asset = _activeAssets[i].asset;
+            uint256 assetBalance = IERC20(asset).balanceOf(address(this));
+            (, int _assetUSDPrice, , , ) = AggregatorV3Interface(_activeAssets[i].aggregator).latestRoundData();
+            assetsTotalValue += assetBalance * uint256(_assetUSDPrice) * 1000 / (PRECISION_E8 * 10**IERC20Metadata(asset).decimals());
+        }
+
+        for (uint256 i = 0; i < _activeAssets.length; i++) {
+            address asset = _activeAssets[i].asset;
+            uint256 assetBalance = IERC20(asset).balanceOf(address(this));
+            (, int _assetUSDPrice, , , ) = AggregatorV3Interface(_activeAssets[i].aggregator).latestRoundData();
+            assetHeldRate[i] = (assetBalance * uint256(_assetUSDPrice) * 1000 / (PRECISION_E8 * 10**IERC20Metadata(asset).decimals())) * 100 / assetsTotalValue;
+
+            uint256 assetPriceForPay = assetHeldRate[i].mul(assetsUSDForPay).mul(PRECISION_E8).mul(10 ** IERC20Metadata(asset).decimals()).div(uint256(_assetUSDPrice).mul(10000));
+            IERC20(asset).transfer(sender, assetPriceForPay);
+        }
     }
 
     /**
@@ -239,6 +334,7 @@ contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
     * @param to Address for ETH to be send to
     * @param value Amount of ETH to send
     */
+
     function _safeTransferETH(address to, uint256 value) internal returns (bool) {
         (bool success, ) = to.call{ value: value, gas: 30_000 }(new bytes(0));
         return success;
@@ -283,19 +379,85 @@ contract Treasury is ERC20, Ownable, Pausable, ReentrancyGuard {
      */
 
     function getKOATTPrice() public view returns(uint256) {
-        return _calcTotalValueOfTreasury() / totalSupply();
+        return _calcTotalValueOfTreasury().div(totalSupply());
     }
+
+    /**
+    * @notice buy KOATT token by Matic
+    */
 
     function buyKOATTETH() external payable {
         require(msg.value >= 0, "Treasury: insufficient funds");
-        uint256 taxFee = msg.value * buyTaxFee / 1000;
-        uint256 usdPriceOfETH = _getMaticUSDPrice(msg.value - taxFee) * 1000 / (PRECISION_E8 * PRECISION);
-        uint256 _mintAmount = usdPriceOfETH / getKOATTPrice();
-        _mint(msg.sender, _mintAmount * 10 ** decimals());
+        uint256 taxFee = msg.value.mul(buyTaxFee).div(1000);
+        uint256 usdPriceOfETH = _getMaticUSDPrice(msg.value.sub(taxFee)).mul(1000).div(PRECISION_E8.mul(PRECISION));
+        uint256 _mintAmount = usdPriceOfETH.div(getKOATTPrice());
+        _mint(msg.sender, _mintAmount.mul(10 ** decimals()));
         _safeTransferETH(teamWallet, taxFee);
     }
 
-    
+    /**
+    * @notice Buy KOATT token by ant coins
+    * @param _antCAmount ant coin amount to buy KOATT token
+    */
+
+    function buyKOATTANTC(uint256 _antCAmount) external {
+        require(IERC20(antCToken).balanceOf(_msgSender()) >= _antCAmount, "Treasury: insufficient ANTC balance");
+        require(IERC20(antCToken).allowance(_msgSender(), address(this)) >= _antCAmount, "Treasury: insufficient ANTC allowance");
+        uint256 taxFee = _antCAmount.mul(buyTaxFee).div(1000);
+        uint256 _antCUSDPrice = _calcANTCUSDValue(_antCAmount.sub(taxFee));
+        uint256 _mintAmount = _antCUSDPrice.div(getKOATTPrice());
+        _mint(msg.sender, _mintAmount.mul(10 ** decimals()));
+        IERC20(antCToken).transferFrom(_msgSender(), address(this), _antCAmount);
+        IERC20(antCToken).transfer(teamWallet, taxFee);
+    }
+
+    /**
+    * @notice Buy KOATT token by asset such as btc or usdt
+    * @param _assetId asset id number of treasury assets
+    * @param _amount asset amount for buying KOATT tokens
+    */
+
+    function buyKOATTAsset(uint256 _assetId, uint256 _amount) external {
+        require(_assetId < _activeAssets.length, "Treasury: invalid asset id");
+        Asset memory _asset = _activeAssets[_assetId];
+        require(IERC20Metadata(_asset.asset).balanceOf(_msgSender()) >= _amount, "Treasury: insufficient asset balance");
+        require(IERC20Metadata(_asset.asset).allowance(_msgSender(), address(this)) >= _amount, "Treasury: insufficient asset allownace");
+        uint256 taxFee = _amount.mul(buyTaxFee).div(1000);
+        uint256 _assetUSDPrice = _getAssetUSDPrice(_assetId);
+        uint256 KOATTPrice = getKOATTPrice();
+        uint256 _mintAmount = _amount.mul(_assetUSDPrice).div(KOATTPrice.mul(1000).mul(10**IERC20Metadata(_asset.asset).decimals()));
+        _mint(_msgSender(), _mintAmount.mul(10 ** decimals()));
+        IERC20Metadata(_asset.asset).transferFrom(_msgSender(), address(this), _amount);
+        IERC20Metadata(_asset.asset).transfer(teamWallet, _amount.sub(taxFee));
+    }
+
+    /**
+    * @notice Sell KOATT token, seller will receive antcoin, antlp, assets
+    * @param _sellAmount sell KOATT token amount
+    */
+
+    function sellKOATTAsset(uint256 _sellAmount) external {
+        address sender = _msgSender();
+        require(balanceOf(sender) >= _sellAmount, "Insufficient KOATT token balance");
+        
+        (uint256 _antCPercentage, uint256 _antCLPPercentage, uint256 _assetsPercentage) = _getCurrentTreasuryHelds();
+        
+        uint256 koattPrice = getKOATTPrice();
+        uint256 totalUSDPriceForPay = _sellAmount.mul(koattPrice).div(10**decimals());
+        uint256 antCUSDForPay = _antCPercentage.mul(totalUSDPriceForPay).div(10000);
+        uint256 antCLPUSDForPay = _antCLPPercentage.mul(totalUSDPriceForPay).div(10000);
+        uint256 assetsUSDForPay = _assetsPercentage.mul(totalUSDPriceForPay).div(10000);
+
+        // ant coin transfer
+        _antCTransfer(antCUSDForPay, sender);
+
+        // ant lp token transfer
+        _antLPTransfer(antCLPUSDForPay, sender);
+
+        // assets transfer
+        _assetsTransfer(assetsUSDForPay, sender);
+    }
+
 
     /**
     *   ██████  ██     ██ ███    ██ ███████ ██████
