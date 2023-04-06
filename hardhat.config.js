ac9312f54c63f4aa2c0c36c7d6ac638372ae2f4f
DreamDev0122 <81429165+DreamDev0122@users.noreply.github.com>
2023-04-06 07:46:50 -0400

    polygon mainnet fork configration

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.13", 
+        version: "0.8.13",
         settings: {
           optimizer: {
             enabled: true,
@@ -16,6 +16,12 @@ module.exports = {
     ],
   },
   networks: {
+    hardhat: {
+      forking: {
+        url: "https://polygon-mainnet.g.alchemy.com/v2/Ph1xM9c5lEPNsTXrO7CJpWzwM5boxO2J",
+        blockNumber: 41062790 
+      }
+    },
     mumbai: {
       chainId: 80001,
       url: "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_URL,
